28e0bd5dca513b088c4499db61ac9965d94a13da
Nômino D Lord <nominodlord@mailfence.com>
2023-11-28 20:48:56 +0100

    Update main.py
    Se añaden:
    - Descripciones sobre cómo usar el archivo.
    - Implementación de módulos para la captura y comparación de las imágenes.

--- main.py
+++ main.py
@@ -8,16 +8,84 @@
 ##                                                                                                                    ##
 ########################################################################################################################
 
+"""
+    IMPORTANTE [LEER ANTES DE MANIPULAR EL CÓDIGO]
+
+    Antes de ejecutar el programa se deben ajustar los siguientes parámetros (muy cuidadosamente):
+
+        - Argumentos para el módulo de capturar_pantalla:
+
+            · Esto dependerá principalmente de la resolución en la que se esté ejecutando el código.
+            · Crear una carpeta donde guardar las imagenes y poder realizar la comparación.
+            · En RUTA_SAVE: Ajustar la ruta de la carpeta donde se guardará la imágen capturada.
+                (Cada vez se guarda una nueva captura se eleminará la primera captura)
+
+        -  Argumentos para el módulo de comparar_imagenes:
+
+            · Ajustar la ruta para la comparación de las dos capturas.
+                + RUTA_IMG_REFERENCIA: Esta imágen será siempre la misma.
+                + RUTA_IMG_CAPTURA: Esta imágen se irá actualizando en cada ronda.
+
+        - Argumentos para las pulsaciones del ratón:
+
+            · Es MUY IMPORTANTE que cuando se ejecute el código no se utilice el PC ya que, de lo contrario,
+              las pulsaciones y las capturas de pantalla no se ejecutarán correctamente. Esto incluye que el
+              movimiento manual del ratón debe estar desactivado para evitar posibles accidentes en el movimiento
+              porque podría perjudicar a la selección de las opciones.
+
+        - Bibliotecas y Módulos:
+
+            · Se deben instalar las bibliotecas adecuadas para poder realizar las importaciones correctamente:
+
+                + Ejecutar en CMD los siguientes comandos...
+
+                    >>> pip3 install Pillow
+                    >>> pip3 install pyautogui
+
+            · Los Módulos deberán estar en la misma carpeta que el archivo principal 'main.py'.
+
+        - Ajustar los parámentros del juego:
+
+            El programa está diseñado para los juegos en el que las opciones de acertar & fallar son entre 1 y 5.
+            Si se quiere ajustar para juegos con un mayor número de opciones, se deberán modificar los siguientes
+            elementos de la función "calcular_apuesta" de este mismo archivo:
+
+                + [ OPCIONES_ACIERTOS / 'Numero' ] → Reducir las opciones a un valor inferior a 5
+                + [ OPCIONES_FALLOS / 'Numero' ] → Reducir las opciones a un valor inferior a 5
+
+                    EJEMPLO: Si las opciones de aciertos y fallos son de 16 y 17 (como es el caso del juego
+                             de la ruleta), el número divisor debería ser un 3 o 4.
+                             (realizar simuladiones para probar antes los resultados).
+"""
+
 # ============================================ [ BIBLIOTECAS & MÓDULOS ] ============================================= #
 
-from random import choice as elegir
 from typing import List
+from time import sleep
+from random import choice as elegir
+
+from capturar_pantalla import captura  # (nombre, ruta, pos_x=0, pos_y=0, ancho=1920, alto=1080)
+from comparar_imagenes import comparar  # (img_referencia, img_capturada, umbral=0.9) -> bool
+from click_raton import pulsar_boton_izquierdo
+
 
 # ================================================== [ VARIABLES ] =================================================== #
 
 global saldo_final
 global saldo
 global lista_resultados
+
+nombre_usuario = Ferran
+# Argumentos para el módulo de capturar_pantalla
+NOMBRE = "captura"
+RUTA_SAVE = f"C:/Users/{nombre_usuario}/Pictures/SCREENS/"
+POS_X, POS_Y = 600, 300
+ANCHO, ALTO = 1080, 600
+
+# Argumentos para el módulo de comparar_imagenes
+RUTA_IMG_REFERENCIA = f"C:/Users/{nombre_usuario}/Pictures/SCREENS/referencia.png"
+RUTA_IMG_CAPTURA = f"C:/Users/{nombre_usuario}/Pictures/SCREENS/captura.png"
+
 
 INDEX = 0
 RONDA = 0
@@ -62,7 +130,7 @@ def generar_lista(resultado) -> List[bool]:
 def actualizar_saldo(apuesta: float, resultado: bool) -> float:
     """
     @param apuesta: Cantidad apostada.
-    @param resultado:
+    @param resultado: bool
         True → Acertar
         False → Fallar
     @return: Saldo final.
@@ -85,6 +153,7 @@ def actualizar_saldo(apuesta: float, resultado: bool) -> float:
         saldo_final += beneficio
         return saldo_final
 
+    pass
 
 def calcular_apuesta(lista, resultado):
 
@@ -102,6 +171,7 @@ def calcular_apuesta(lista, resultado):
     if resultado is False and len(lista_variable_falses) > 0:
         lista_variable_falses.pop()
 
+
     listas_trues_falses = [lista, lista_variable_trues, lista_variable_falses]
     total_trues, total_falses = contar_valores(listas_trues_falses)
 
@@ -109,18 +179,20 @@ def calcular_apuesta(lista, resultado):
     proporcion_falses = total_falses * OPCIONES_ACIERTOS
 
     if proporcion_trues > proporcion_falses:
-        apostar = OPCIONES_ACIERTOS
+        apostar = OPCIONES_ACIERTOS * 20
         return apostar
 
-    if proporcion_trues < proporcion_falses:
-        apostar = OPCIONES_FALLOS
+    elif proporcion_trues < proporcion_falses:
+        apostar = OPCIONES_FALLOS * 3
         return apostar
 
-    if proporcion_trues == proporcion_falses:
-        apostar = OPCIONES_TOTALES / 2
+    else:
+        apostar = OPCIONES_TOTALES
         return apostar
 
     pass
+
+
 
 # ==================================================== [ PRUEBAS ] =================================================== #
 
@@ -162,19 +234,36 @@ def generar_resultado(trues: int, falses: int) -> bool:
 
 lista_variable_trues, lista_variable_falses = generar_listas_100()
 
-repetir = 1000  # TODO: PRUEBAS
+# sleep(seg)
+"""
+    ¡¡¡MUY IMPORTANTE!!!
+
+    Ajustar MUY cuidadosamente los elementos dentro del bucle 'while' antes de ejecutar el código.
+    
+    Los datos de las posiciones al realizar las capturas de pantalla y las pulsaciones del ratón dependerán
+    de la resolución de pantalla en la que es ejecutada, por lo que, es de vital importancia realizar antes
+    las pruebas necesarias para ajustar estos parámetros.
+    
+"""
+
 while True:
 
-    # repetir -= 1
     rondas = contar_rondas()
-    # resultado_ronda = input("INSERTAR EL RESULTADO EN RONDA: ")
-    resultado_ronda = generar_resultado(OPCIONES_ACIERTOS, OPCIONES_FALLOS)  # TODO: PRUEBAS
+
+    captura(NOMBRE, RUTA_SAVE, POS_X, POS_Y, ANCHO, ALTO)
+    resultado_ronda = not comparar(RUTA_IMG_REFERENCIA, RUTA_IMG_CAPTURA)
+    """
+    NOTA:
+    La imagen de referencia es una captura inicial cuando NO has acertado, por ese motivo,
+    se añade 'not' en la salida del resultado para la función 'comparar' del módulo 'comprar_imagenes'.
+    """
+    # resultado_ronda = generar_resultado(OPCIONES_ACIERTOS, OPCIONES_FALLOS)  # PRUEBAS
 
     lista_definida = generar_lista(resultado_ronda)
 
     # indice = generar_indice(resultado_ronda)
 
-    cantidad_apostada = calcular_apuesta(lista_definida, resultado_ronda)  # resultado_ronda (Segundo arg en Prueba 1)
+    cantidad_apostada = calcular_apuesta(lista_definida, resultado_ronda)
     saldo = actualizar_saldo(cantidad_apostada, resultado_ronda)
 
     print(f"RONDA: {rondas}")
@@ -183,10 +272,8 @@ while True:
     print(f"SALDO → {saldo}")
     print("--------------------------")
 
-    if saldo < 1:
+    if saldo < 100 or rondas == 10000 or (saldo - SALDO_INICIAL) > 1000:
         break
 
-    if (saldo - SALDO_INICIAL) > 10000:
-        break
 
 print(f"Beneficios → {saldo - SALDO_INICIAL}")
