8efb3bdc510cfbb4fd44fd8e068776257480c4ab
Nômino D Lord <nominodlord@mailfence.com>
2023-12-28 19:29:35 +0100

    Re-estructuracion

--- test/Estrategias/estrategia_14.py
+++ test/Estrategias/estrategia_14.py
@@ -24,6 +24,7 @@ beneficio_max = 0
 perdida_max = 0
 
 multiplicador = 2
+incremento = 1
 
 contar_true = 0
 contar_false = 0
@@ -40,7 +41,7 @@ fallos_posicion_false = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 
 lista_beneficios = []
 lista_perdidas = []
-
+lista_valores = []
 
 ronda = 0
 rondas = 100000
@@ -53,43 +54,42 @@ while rondas != ronda:
 
     else:
 
+        suma_fallos = sum(fallos_posicion_false)
+
+        repartir_cantidades = suma_fallos / len(posiciones_false)
+
+        for _ in posiciones_false:
+            lista_valores.append(repartir_cantidades)
+
         if perdidas > 0:
 
             longitud_lista_perdidas = len(posiciones_false)
             repartir_perdidas = round((perdidas / longitud_lista_perdidas), 2)
 
-            for _ in posiciones_false:
-                lista_perdidas.append(repartir_perdidas)
-                # repartir_perdidas *= 1.3
+            if resultado is False:
 
-        else:
+                apuesta = repartir_perdidas * repeticion_false
 
-            longitud_lista_beneficios = len(posiciones_true)
-            repartir_beneficios = round((perdidas / longitud_lista_beneficios), 2)
+            if resultado is True:
+                apuesta = repartir_perdidas / repeticion_true
 
-            for posicion in posiciones_true:
-                lista_beneficios.append(repartir_beneficios)
-                repartir_beneficios *= 0.5
+        if beneficios > 0:
 
-        if resultado is False:
-            if perdidas > 0:
-                apuesta = lista_perdidas[repeticion_false]
-            else:
-                apuesta = limite_apuesta_minima
+            if ronda > 100:
 
-        if resultado is True:
-            if perdidas > 0:
+                proporcion_false = contar_false / ronda
+                proporcion_true = contar_true / ronda
 
-                lista_perdidas_recortada = lista_perdidas[:10]
-                lista_perdidas_recortada.sort(reverse=True)
+                if proporcion_false > 0.56:
+                    incremento = 2
 
-                if repeticion_true < 10:
-                    apuesta = lista_perdidas_recortada[repeticion_true]
-                else:
-                    apuesta = limite_apuesta_minima
+                if proporcion_false > 0.53 and incremento == 2:
+                    incremento = 1
 
-            else:
-                apuesta = lista_beneficios[repeticion_true]
+            apuesta = 1 * incremento
+
+
+    # --------------------------------------------------------------------
 
     if apuesta < limite_apuesta_minima:
         apuesta = limite_apuesta_minima
@@ -98,10 +98,10 @@ while rondas != ronda:
 
         proporcion_false = contar_false / ronda
 
-        if proporcion_false > 0.60:
+        if proporcion_false > 60:
             apuesta *= 3
 
-        elif proporcion_false > 0.55:
+        elif proporcion_false > 55:
             apuesta *= 2
 
     apuesta = round(apuesta, 2)
@@ -113,7 +113,7 @@ while rondas != ronda:
     if resultado is False:
         contar_false += 1
         repeticion_false += 1
-        fallos_posicion_false[repeticion_false - 1] += 1
+        fallos_posicion_false[repeticion_false - 1] += apuesta
 
         if repeticion_true != 0:
             repeticion_true = 0
@@ -125,7 +125,7 @@ while rondas != ronda:
         if repeticion_false != 0:
 
             posiciones_false[repeticion_false - 1] += 1
-            fallos_posicion_false[repeticion_false - 1] -= 1
+            fallos_posicion_false[repeticion_false - 1] -= (apuesta * multiplicador) - apuesta
 
         repeticion_false = 0
 
